0cdaf342427de9e32c8a71ab162df7a1fbcd9778
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-03 14:42:10 +0900

    InsufficientBalanceException add

--- src/workshop/account/exception/InsufficientBalanceException.java
+++ src/workshop/account/exception/InsufficientBalanceException.java
@@ -1,7 +1,17 @@
 package workshop.account.exception;
 
+/*
+ * Account의 잔액(balance)이 부족할때 사용되는 사용자정의 Exception 클래스입니다. * 
+ */
 public class InsufficientBalanceException extends Exception {
-	public InsufficientBalanceException(String errMessage) {
-		
+	private int currentBalance;
+	
+	public InsufficientBalanceException(String errMessage, int currentBalance) {
+		super(errMessage);
+		this.currentBalance = currentBalance;
+	}
+	
+	public int getCurrentBalance() {
+		return currentBalance;
 	}
 }

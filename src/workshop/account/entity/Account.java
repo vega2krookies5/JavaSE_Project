ae8ae7acdefa0caaa7f13796338537a6f066bd25
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-03 13:20:01 +0900

    Account setter getter add

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -5,4 +5,32 @@ public class Account {
 	private String acctId;
 	private int balance;
 	
+	//setter method
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	//getter method
+	public String getCustId() {
+		return custId;
+	}
+
+	public String getAcctId() {
+		return acctId;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	
+	
 }

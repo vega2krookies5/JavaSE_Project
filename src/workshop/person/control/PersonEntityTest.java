17e2d0b70fd1e82db233941be71e1b325080301b
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-03 15:45:59 +0900

    PersonEntityTest update

--- src/workshop/person/control/PersonEntityTest.java
+++ src/workshop/person/control/PersonEntityTest.java
@@ -4,19 +4,22 @@ import workshop.person.entity.PersonEntity;
 
 public class PersonEntityTest {
 	public static void main(String[] args) {
+		PersonEntityTest test = new PersonEntityTest();
+		
 		PersonEntity[] persons = new PersonEntity[2];
-		//persons[0] 의 타입은?? PersonEntity 타입
-		persons[0] = new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932");
-		persons[1] = new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932");
 		
-//		for (int i = 0; i < persons.length; i++) {
-//			
-//		}
-		
+		test.fillPersons(persons);
+				
 		//Enhanced for Loop		
 		for (PersonEntity person : persons) {
 			System.out.println(person.getName() + " " + person.getGender());
 		}
 		
+	}
+
+	public void fillPersons(PersonEntity[] persons) {
+		//persons[0] 의 타입은?? PersonEntity 타입
+		persons[0] = new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932");
+		persons[1] = new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932");
 	}
 }

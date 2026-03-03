8669191e9039a609c3d3e25f0832aea6c28300de
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-03 13:35:24 +0900

    AccountTest add

---
+++ src/workshop/account/control/AccountTest.java
@@ -0,0 +1,20 @@
+package workshop.account.control;
+
+import workshop.account.entity.Account;
+
+public class AccountTest {
+	public static void main(String[] args) {
+		//Account °´Ã¼»ı¼º
+		Account account = new Account();
+		//°í°´¹øÈ£ : A1100, °èÁÂ¹øÈ£ : 221-22-3477, ÀÜ¾× : 100000
+		account.setCustId("A1100");
+		account.setAcctId("221-22-3477");
+		account.deposit(100000);
+		
+		System.out.println("°í°´¹øÈ£ : " + account.getCustId());
+		System.out.println("°èÁÂ¹øÈ£ : " + account.getAcctId());
+		System.out.println("ÀÜ¾× :" + account.getBalance());
+
+		
+	}
+}

7b44ef5576baffe07e5bb646d495638122b2c425
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-03 15:40:20 +0900

    PersonEntityTest update

--- src/workshop/person/control/PersonEntityTest.java
+++ src/workshop/person/control/PersonEntityTest.java
@@ -9,5 +9,14 @@ public class PersonEntityTest {
 		persons[0] = new PersonEntity("이성호","7212121028102", "인천 계양구", "032-392-2932");
 		persons[1] = new PersonEntity("김하늘","7302132363217", "서울 강동구", "02-362-1932");
 		
+//		for (int i = 0; i < persons.length; i++) {
+//			
+//		}
+		
+		//Enhanced for Loop		
+		for (PersonEntity person : persons) {
+			System.out.println(person.getName() + " " + person.getGender());
+		}
+		
 	}
 }

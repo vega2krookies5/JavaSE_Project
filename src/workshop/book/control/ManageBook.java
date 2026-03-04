e800436803f6ce7e48969bc98484f617b6e84e60
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-04 10:27:43 +0900

    ManageBook update

--- src/workshop/book/control/ManageBook.java
+++ src/workshop/book/control/ManageBook.java
@@ -8,7 +8,28 @@ import workshop.book.entity.ReferenceBook;
 public class ManageBook {
 
 	public static void main(String[] args) {
-		//Publication 타입 배열을 선언 및 생성
+		Magazine pub1 = new Magazine(); 
+		//Publication method
+		pub1.getPage();
+		pub1.getPrice();
+		pub1.getPublishingDate();
+		pub1.getTitle();
+		//Magazine method
+		pub1.getPublishingPeriod();
+		
+		//Poly(다양한) + morphism(변형)		
+		Publication pub = new Magazine();
+		//Publication method
+		pub.getPage();
+		pub.getPrice();
+		pub.getPublishingDate();
+		pub.getTitle();
+		
+//		Magazine mag = (Magazine)pub;
+//		mag.getPublishingPeriod();
+		((Magazine)pub).getPublishingPeriod();
+		
+		//Publication 타입 배열을 선언 및 생성 
 		Publication[] pubs = new Publication[5];
 		pubs[0] = new Magazine("마이크로소프트","2007-10-01",328,9900,"매월");
 		pubs[1] = new Magazine("경영과컴퓨터","2007-10-03",316,9000,"매월");
